b3559e9445c461f1ec96e150eea2a49af6364e30
FamishedRook871 <79121610+FamishedRook871@users.noreply.github.com>
2021-06-07 20:59:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+h1
+{
+	color: red; 
+	font-size: 40px;
+  
+}
+.body_backgorund {
+	background-image: url("gty_592597166_84595138")
+	background-position: center;
+	background-size: cover;
+}
+
+#myCanvas{
+	border-width: 10px;
+	background-color: white;
+	border-style: groove;
+  background-image: url("UsainBolt-58dbe5253df78c516232331c")
+}
+span{
+	font-size: 50px;
+}
